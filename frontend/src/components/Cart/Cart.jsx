c94a3dce8d4e19dea4e859ec680db5b0f2844e4a
OYusuf <olayusuf822@gmail.com>
2024-09-13 15:12:22 -0500

    cart navbar implemented

--- frontend/src/components/Cart/Cart.jsx
+++ frontend/src/components/Cart/Cart.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <div>
+      <h1>Your Cart</h1>
+      <p>Your cart is currently empty.</p>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
