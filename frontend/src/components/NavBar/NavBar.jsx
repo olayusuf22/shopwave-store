9757c4aa9414ff1f6813ac7b762ae3c74ee2d6ed
OYusuf <olayusuf822@gmail.com>
2024-09-18 15:52:48 -0500

    logo rename

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import * as authService from '../../services/authService';
 import './NavBar.css';
-import logo from '../../assets/Logo.png';
+import logo from '../../assets/logo.png';
 
 export default function NavBar({ user, setUser }) {
   function handleLogOut() {
